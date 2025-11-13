42eae95cef8f6ce6262e4e7913fe0072d1342413
daro <mainworkai@gmail.com>
2025-11-13 08:14:46 +0100

    First commit

---
+++ AlienUniverse/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AlienUniverse.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ AlienUniverse/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AlienUniverse.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
